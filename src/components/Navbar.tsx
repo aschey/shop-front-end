bd5737c977dcc19c644d65e42b8bb5c17d3e440c
Amie Wei <amie.wei12@gmail.com>
2023-03-22 22:10:21 -0500

    updated link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -111,35 +111,38 @@ export default function Navbar() {
               Log in
             </a> */}
             {/* ---------------Shopping Cart Button START------------*/}
-            <a
-              href="/cart"
-              className="relative mr-2 rounded-lg bg-secondary-600 px-4 py-2 text-sm font-medium text-white hover:bg-primary-400 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 lg:px-5 lg:py-2.5"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-                strokeWidth={1.8}
-                stroke="currentColor"
-                className="h-6 w-7"
+            <>
+              <Link
+                to="/cart"
+                className="relative mr-2 rounded-lg bg-secondary-600 px-4 py-2 text-sm font-medium text-white hover:bg-primary-400 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 lg:px-5 lg:py-2.5"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-                />
-              </svg>
-              {itemCount ? (
-                <>
-                  <span className="absolute right-[-5px] bottom-[-5px] flex h-5 w-5">
-                    <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-secondary-300 opacity-75"></span>
-                    <span className="relative inline-flex h-5 w-5 items-center justify-center rounded-full bg-secondary-300 text-xs text-white">
-                      {itemCount}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                  strokeWidth={1.8}
+                  stroke="currentColor"
+                  className="h-6 w-7"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                  />
+                </svg>
+                {itemCount ? (
+                  <>
+                    <span className="absolute right-[-5px] bottom-[-5px] flex h-5 w-5">
+                      <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-secondary-300 opacity-75"></span>
+                      <span className="relative inline-flex h-5 w-5 items-center justify-center rounded-full bg-secondary-300 text-xs text-white">
+                        {itemCount}
+                      </span>
                     </span>
-                  </span>
-                </>
-              ) : null}
-            </a>
+                  </>
+                ) : null}
+              </Link>
+            </>
+
             {/* ---------------Shopping Cart Button END------------*/}
             <button
               data-collapse-toggle="mobile-menu-2"
